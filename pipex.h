f94f4c70423e506f8e4405cea6d2271ea76823a9
mmoramova <moramova@gmail.com>
2023-02-12 18:55:41 +0100

    added libft, pipex.h

---
+++ pipex.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoramov <mmoramov@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/05 11:17:56 by mmoramov          #+#    #+#             */
+/*   Updated: 2023/02/12 18:55:04 by mmoramov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <fcntl.h> //open access
+# include <unistd.h> //open close read write access dup dup2 execve fork pipe
+# include <stdlib.h> // free
+# include "libft/libft.h"
+
+#endif
