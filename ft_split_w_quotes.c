d384a983e7aeb8f472ff7fd85c2583343c0f170a
mmoramova <moramova@gmail.com>
2023-05-03 22:43:24 +0200

    works for escaped char

--- ft_split_w_quotes.c
+++ ft_split_w_quotes.c
@@ -6,7 +6,7 @@
 /*   By: mmoramov <mmoramov@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 14:51:15 by mmoramov          #+#    #+#             */
-/*   Updated: 2023/05/01 19:12:20 by mmoramov         ###   ########.fr       */
+/*   Updated: 2023/05/03 21:53:36 by mmoramov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,16 +37,55 @@ int	ft_wordcount_w_quotes(char const *s, char c)
 	return (nbr);
 }
 
-int	ft_wordlen_w_quotes(char const *s, char c)
+char ft_set_quote(char actQuote)
 {
-	int		i;
 	char	singlequote;
 	char	doublequote;
-	char	actQuote;
-	int		nestednbr;
+	char	newQuote;
 
 	singlequote = '\'';
 	doublequote = '\"';
+	if (actQuote == singlequote)
+		newQuote = doublequote;
+	else
+		newQuote = singlequote;
+	return(newQuote);
+}
+
+char *ft_substr_wo_escapedchar(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	len_s;
+	unsigned int	i;
+	unsigned int	j;
+	char			*p;
+
+	len_s = ft_strlen(s);
+	i = 0;
+	j = 0;
+	if (len_s <= start)
+		len = 0;
+	else if (len_s < start + len)
+		len = len_s - start;
+	p = malloc(sizeof(char) * (len + 1));
+	if (!p)
+		return (NULL);
+	while (s[i] && len--)
+	{
+		if (s[start + i + j] == '\\')
+			j += 1;
+		p[i] = s[start + i + j];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
+int	ft_wordlen_w_quotes(char const *s, char c)
+{
+	int		i;
+	char	actQuote;
+	int		nestednbr;
+
 	actQuote = '\0';
 	nestednbr = 0;
 	i = 0;
@@ -54,28 +93,49 @@ int	ft_wordlen_w_quotes(char const *s, char c)
 	{
 		if (s[i] == c && actQuote == '\0')
 			return(i);
-		if (s[i] == singlequote || s[i] == doublequote)
+		if (ft_strchr("\'\"", s[i]))
 		{
-			if(actQuote != '\0')
+			if(actQuote != '\0' && s[i] == actQuote)
 			{
-				if (s[i] == actQuote)
-				{
-					nestednbr -=1;
-					if (nestednbr == 0)
-						actQuote = '\0';
-					else
-					{
-						if (actQuote == singlequote)
-							actQuote = doublequote;
-						else
-							actQuote = singlequote;
-					}
-				}
+				nestednbr -=1;
+				if (nestednbr == 0)
+					actQuote = '\0';
 				else
-				{
-					actQuote = s[i];
-					nestednbr +=1;
-				}
+					actQuote = ft_set_quote(actQuote);
+			}
+			else
+			{
+				actQuote = s[i];
+				nestednbr +=1;
+			}
+		}
+		i++;
+	}
+	return (i);
+}
+
+int	ft_wordlen_w_quotes_wo_escapedchar(char const *s, char c)
+{
+	int		i;
+	char	actQuote;
+	int		nestednbr;
+
+	actQuote = '\0';
+	nestednbr = 0;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c && actQuote == '\0')
+			return(i);
+		if (ft_strchr("\'\"", s[i]) && (s[i-1] != '\\'))
+		{
+			if(actQuote != '\0' && s[i] == actQuote)
+			{
+				nestednbr -=1;
+				if (nestednbr == 0)
+					actQuote = '\0';
+				else
+					actQuote = ft_set_quote(actQuote);
 			}
 			else
 			{
@@ -113,12 +173,13 @@ char	**ft_split_w_quotes(char *s, char c)
 	{
 		if (s[i] != c)
 		{
-			p[j] = ft_substr(s, start, ft_wordlen_w_quotes(&s[i], c));
+			p[j] = ft_substr_wo_escapedchar(s, start, ft_wordlen_w_quotes_wo_escapedchar(&s[i], c));
 			if (!p[j])
 			{
 				ft_free_w_quotes(p, j);
 				return (NULL);
 			}
+
 			j++;
 			i += (ft_wordlen_w_quotes(&s[i], c) - 1);
 			start = i+1;
@@ -131,9 +192,10 @@ char	**ft_split_w_quotes(char *s, char c)
 	return (p);
 }
 
-/*int main(void)
+/*
+int main(void)
 {
-	char *s  = "awk \"{count++} END {printf \"count:%i\" , count}\"";
+	char *s  = "awk \"{count++} END {printf \\\"count:%i\\\" , count}\"";
 
 	 char **t;
 	 t = ft_split_w_quotes(s, ' ');
@@ -146,4 +208,5 @@ char	**ft_split_w_quotes(char *s, char c)
 		i++;
 	}
 	//gcc ft_split_w_quotes.c libft/libft.a && ./a.out
-}*/
+}
+*/
